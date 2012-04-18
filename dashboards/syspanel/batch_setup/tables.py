a3b8ba75d916ff6fe13e5c7a3710aafd34c9761b
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2012-04-18 08:38:56 +0200

    Added delete batch. testing first. Nothing will actually be deleted

--- dashboards/syspanel/batch_setup/tables.py
+++ dashboards/syspanel/batch_setup/tables.py
@@ -28,7 +28,35 @@ class DeleteBatch(tables.DeleteAction):
     data_type_plural = _("Batches")
 
     def delete(self, request, obj_id):
-        LOG.info("got back in deletebatch : %s"% obj_id)
+        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="melkikakao2012", db="dash")
+        cursor = db.cursor()
+        cursor.execute( "SELECT tenant_id FROM batch_tenants WHERE batch_id='%s'" % obj_id )
+        tenants = cursor.fetchall()
+        instances = []
+	try:
+            instances = api.nova.server_list(self.request, all_tenants=True)
+	except:
+            exceptions.handle(self.request, _('Unable to retrieve instance list.'))
+
+	if instances:
+            for tenant in tenants:									# For every project
+                for inst in instances:								# Search every single instance
+                    if inst.tenant_id == tenant[0]:					# Find those instances that belong to the project
+                        LOG.info("Will delete instance %s" % inst.id)	
+			#api.nova.server_delete(request, inst.id)	# Delete them
+			users = api.keystone.user_list(request, tenant_id=tenant)	# Fetch all users in the project
+			for user in users:											
+                            LOG.info("will remove %s from %s" % (user.name, tenant) )				
+				#api.keystone.remove_tenant_user(request, tenant, user.id)	# Remove their roles in the project
+                            if not user.name == "admin":							# Delete all users but admin
+                                LOG.info("Deleting user %s" % user.name)					
+                                #api.keystone.user_delete(request, user.id)
+			# Scrub project
+			LOG.info("Deleting tenant %s" % tenant)			
+			#api.keystone.tenant_delete(request, tenant)	# Delete tenant
+        else:
+            LOG.info("no instances..!")
+                                
 
 
 class BatchOverview(tables.DataTable):
