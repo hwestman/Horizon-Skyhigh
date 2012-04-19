c058e5433f24756bfa4ca028153a52d4c3777962
Hallvard Westman <hwestman@ross.(none)>
2012-04-19 10:41:19 +0200

    changed template

--- dashboards/syspanel/batch_setup/tables.py
+++ dashboards/syspanel/batch_setup/tables.py
@@ -87,7 +87,7 @@ class BatchOverview(tables.DataTable):
 	class Meta:
 		name = "batch_overview"
 		verbose_name = _("Batches")
-		table_actions = (DeleteBatch, )
+		table_actions = (DeleteBatch,EditBatchLink, )
 		row_actions = (EditBatchLink,DeleteBatch, )
 
 class InstanceSetup(tables.DataTable):
