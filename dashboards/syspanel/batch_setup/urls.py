f83307cc212817b2bc85b4ee92296ae1eadfd744
Hallvard Westman <hwestman@ross.(none)>
2012-04-19 11:24:17 +0200

    changed template

--- dashboards/syspanel/batch_setup/urls.py
+++ dashboards/syspanel/batch_setup/urls.py
@@ -26,3 +26,6 @@ urlpatterns = patterns('horizon.dashboards.syspanel.batch_setup.views',
 	url(r'^create_batch/$', CreateBatchView.as_view(), name='create_batch'),
 	url(r'^add_instance/$', AddInstanceView.as_view(), name='add_instance'),
 	url(r'^(?P<batch_id>[^/]+)/edit_batch/$', EditBatchView.as_view(), name='edit_batch'),)
+
+
+	
